ffc5cf6c62a6614c6a592abfc19fb1fcd9ede9ef
Ellen Lee <ellen811003@gmail.com>
2019-09-26 15:07:23 +0800

    Updates

---
+++ precache-manifest.26eeebe48b4aebd7c41f07c2509cdef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "158cc3afa8b96a1449e9bfb2533c9d4b",
+    "url": "/react-user-app/index.html"
+  },
+  {
+    "revision": "c5fb293a96414cee12da",
+    "url": "/react-user-app/static/css/2.c0851dc3.chunk.css"
+  },
+  {
+    "revision": "f97bd46dbcad40442a77",
+    "url": "/react-user-app/static/css/main.b592a723.chunk.css"
+  },
+  {
+    "revision": "c5fb293a96414cee12da",
+    "url": "/react-user-app/static/js/2.14d57e1a.chunk.js"
+  },
+  {
+    "revision": "f97bd46dbcad40442a77",
+    "url": "/react-user-app/static/js/main.df2ade55.chunk.js"
+  },
+  {
+    "revision": "92487c688ed2dc2d74c9",
+    "url": "/react-user-app/static/js/runtime-main.3dcb72df.js"
+  }
+]);
